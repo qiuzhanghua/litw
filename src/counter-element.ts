33f39b16b696a3f4105e6468457a9ad2c7b4bc55
qiuzhanghua <qiuzhanghua@icloud.com>
2023-04-06 17:40:38 +0800

    rename my -> counter, clean

--- src/counter-element.ts
+++ src/counter-element.ts
@@ -1,7 +1,5 @@
 import { LitElement, css, html } from 'lit'
 import { customElement, property } from 'lit/decorators.js'
-import litLogo from './assets/lit.svg'
-import viteLogo from '/vite.svg'
 
 /**
  * An example element.
@@ -9,13 +7,13 @@ import viteLogo from '/vite.svg'
  * @slot - This element has a slot
  * @csspart button - The button
  */
-@customElement('my-element')
-export class MyElement extends LitElement {
+@customElement('counter-element')
+export class CounterElement extends LitElement {
   /**
    * Copy for the read the docs hint.
    */
   @property()
-  docsHint = 'Click on the Vite and Lit logos to learn more'
+  docsHint = 'Use the button to increment the counter.'
 
   /**
    * The number of times the button has been clicked.
@@ -25,14 +23,6 @@ export class MyElement extends LitElement {
 
   render() {
     return html`
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src=${viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://lit.dev" target="_blank">
-          <img src=${litLogo} class="logo lit" alt="Lit logo" />
-        </a>
-      </div>
       <slot></slot>
       <div class="card">
         <button @click=${this._onClick} part="button" class="text-red-500">
@@ -123,6 +113,6 @@ export class MyElement extends LitElement {
 
 declare global {
   interface HTMLElementTagNameMap {
-    'my-element': MyElement
+    'counter-element': CounterElement
   }
 }
--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ import ViteInspector from 'vite-plugin-inspect'
 export default defineConfig({
   build: {
     lib: {
-      entry: 'src/my-element.ts',
+      entry: 'src/counter-element.ts',
       formats: ['es'],
     },
     rollupOptions: {
