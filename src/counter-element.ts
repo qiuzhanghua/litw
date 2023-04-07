549480d3570d58c55ca40780a85304ea19351f2d
qiuzhanghua <qiuzhanghua@icloud.com>
2023-04-07 15:22:38 +0800

    add an html wc

--- src/counter-element.ts
+++ src/counter-element.ts
@@ -1,5 +1,5 @@
-import { LitElement, css, html } from 'lit'
-import { customElement, property } from 'lit/decorators.js'
+import {LitElement, css, html} from 'lit'
+import {customElement, property} from 'lit/decorators.js'
 
 /**
  * An example element.
@@ -9,111 +9,133 @@ import { customElement, property } from 'lit/decorators.js'
  */
 @customElement('counter-element')
 export class CounterElement extends LitElement {
-  /**
-   * Copy for the read the docs hint.
-   */
-  @property()
-  docsHint = 'Use the button to increment the counter.'
+    /**
+     * Copy for the read the docs hint.
+     */
+    @property()
+    docsHint = 'Use the button to increment the counter.'
 
-  /**
-   * The number of times the button has been clicked.
-   */
-  @property({ type: Number })
-  count = 0
+    /**
+     * The number of times the button has been clicked.
+     */
+    @property({type: Number})
+    count = 0
 
-  render() {
-    return html`
-      <slot></slot>
-      <div class="card">
-        <button @click=${this._onClick} part="button" class="text-red-500">
-          count is ${this.count}
-        </button>
-        <div class="i-ic-baseline-add-circle text-3xl bg-green-500" />
-      </div>
-      <p class="cool-blue">${this.docsHint}</p>
-    `
-  }
+    @property()
+    span = false
 
-  private _onClick() {
-    this.count++
-  }
-
-  static styles = css`
-    :host {
-      max-width: 1280px;
-      margin: 0 auto;
-      padding: 2rem;
-      text-align: center;
-    }
-
-    .logo {
-      height: 6em;
-      padding: 1.5em;
-      will-change: filter;
-      transition: filter 300ms;
-    }
-    .logo:hover {
-      filter: drop-shadow(0 0 2em #646cffaa);
-    }
-    .logo.lit:hover {
-      filter: drop-shadow(0 0 2em #325cffaa);
+    render() {
+        return html`
+            <span class="logo"></span>
+            ${this.span ? html`
+                <div class="bg-red-400">BG Color should change</div>` : html`
+                <div>BG Color should change</div>`}
+            <button @click=${this._toggleSpan} part="button">
+                Change BG Color:: ${this.span ? 'Normal' : 'Red'}
+            </button>
+            <slot></slot>
+            <div class="card">
+                <button @click=${this._onClick} part="button" class="text-red-500">
+                    count is ${this.count}
+                </button>
+                <div class="i-ic-baseline-add-circle text-3xl bg-green-500"/>
+            </div>
+            <p class="cool-blue">${this.docsHint}</p>
+            <another-element class="part-[cool-part]:cool-green part-[another-cool-part]:cool-green">Other Element
+            </another-element>
+        `
     }
 
-    .card {
-      padding: 2em;
+    private _onClick() {
+        this.count++
     }
 
-    .read-the-docs {
-      color: #888;
+    private _toggleSpan() {
+        this.span = !this.span
     }
 
-    h1 {
-      font-size: 3.2em;
-      line-height: 1.1;
-    }
+    static styles = css`
+      :host {
+        max-width: 1280px;
+        margin: 0 auto;
+        padding: 2rem;
+        text-align: center;
+      }
 
-    a {
-      font-weight: 500;
-      color: #646cff;
-      text-decoration: inherit;
-    }
-    a:hover {
-      color: #535bf2;
-    }
+      .logo {
+        height: 6em;
+        padding: 1.5em;
+        will-change: filter;
+        transition: filter 300ms;
+      }
 
-    button {
-      border-radius: 8px;
-      border: 1px solid transparent;
-      padding: 0.6em 1.2em;
-      font-size: 1em;
-      font-weight: 500;
-      font-family: inherit;
-      background-color: #1a1a1a;
-      cursor: pointer;
-      transition: border-color 0.25s;
-    }
-    button:hover {
-      border-color: #646cff;
-    }
-    button:focus,
-    button:focus-visible {
-      outline: 4px auto -webkit-focus-ring-color;
-    }
+      .logo:hover {
+        filter: drop-shadow(0 0 2em #646cffaa);
+      }
 
-    @media (prefers-color-scheme: light) {
+      .logo.lit:hover {
+        filter: drop-shadow(0 0 2em #325cffaa);
+      }
+
+      .card {
+        padding: 2em;
+      }
+
+      .read-the-docs {
+        color: #888;
+      }
+
+      h1 {
+        font-size: 3.2em;
+        line-height: 1.1;
+      }
+
+      a {
+        font-weight: 500;
+        color: #646cff;
+        text-decoration: inherit;
+      }
+
       a:hover {
-        color: #747bff;
+        color: #535bf2;
       }
+
       button {
-        background-color: #f9f9f9;
+        border-radius: 8px;
+        border: 1px solid transparent;
+        padding: 0.6em 1.2em;
+        font-size: 1em;
+        font-weight: 500;
+        font-family: inherit;
+        background-color: #1a1a1a;
+        cursor: pointer;
+        transition: border-color 0.25s;
       }
-    }
-  @unocss-placeholder
-  `
+
+      button:hover {
+        border-color: #646cff;
+      }
+
+      button:focus,
+      button:focus-visible {
+        outline: 4px auto -webkit-focus-ring-color;
+      }
+
+      @media (prefers-color-scheme: light) {
+        a:hover {
+          color: #747bff;
+        }
+
+        button {
+          background-color: #f9f9f9;
+        }
+      }
+    @unocss-placeholder
+    `
 }
 
 declare global {
-  interface HTMLElementTagNameMap {
-    'counter-element': CounterElement
-  }
+    interface HTMLElementTagNameMap {
+        'counter-element': CounterElement
+    }
 }
